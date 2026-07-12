b2ca8da6edafe56abdefc1b24c3f0d481e4b84af
Aniketh Singh <aniketh9410280466@gmail.com>
2026-07-12 13:37:47 +0530

    added crud of driver

--- backend/src/routes/driver.routes.js
+++ backend/src/routes/driver.routes.js
@@ -2,12 +2,12 @@ import express from "express";
 const router = express.Router();
 
 import { getAllDrivers, getDriverById, createDriver, updateDriver, deleteDriver } from "../controllers/driverController.js";
-
+import { protect, authorize } from "../middlewares/authmiddleware.js";
 // Driver Routes
-router.get("/", getAllDrivers);
-router.get("/:id", getDriverById);
-router.post("/", createDriver);
-router.patch("/:id", updateDriver);
-router.delete("/:id", deleteDriver);
+router.get("/", protect, authorize("FleetManager"), getAllDrivers);
+router.get("/:id", protect, authorize("FleetManager"), getDriverById);
+router.post("/", protect, authorize("FleetManager"), createDriver);
+router.patch("/:id", protect, authorize("FleetManager"), updateDriver);
+router.delete("/:id", protect, authorize("FleetManager"), deleteDriver);
 
 export default router;
